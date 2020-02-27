4914b1279374a942d26bf86dc666f9a6944043d3
DarkSheikah <36537284+DarkSheikah@users.noreply.github.com>
2020-02-27 22:37:08 +0100

    select menu

--- select.js
+++ select.js
@@ -1,4 +1,15 @@
-function backGroundColor(attr, valu) {
-	var elem = document.body;
-	elem.classList.toggle("bgColor");
+// Werkend maken met FireFox
+// Heeft net iets andere rules
+
+function changeAppearance(attr, prop, valu) {
+	var design = document.getElementById("design").sheet;
+
+	if(design.rules[0].selectorText == ".appearance") {
+		design.removeRule(0);
+	}
+
+	design.insertRule('.appearance {' + prop + ': ' + valu + ';}', 0);
+
+	var elem = document.getElementsByTagName(attr)[0];
+	elem.classList.toggle("appearance");
 }
