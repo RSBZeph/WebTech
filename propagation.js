ce263015dc1ade36700570be1dbe29da3ec93735
DarkSheikah <36537284+DarkSheikah@users.noreply.github.com>
2020-03-06 14:25:18 +0100

    propagation and random fixes

---
+++ propagation.js
@@ -0,0 +1,10 @@
+document.getElementById("propChild").addEventListener("click", a, false);
+document.getElementById("propParent").addEventListener("click", b, false);
+
+function a() {
+	alert("This is an alert for the button")
+}
+
+function b() {
+	alert("This is an alert for the parent of the button")
+}
